83d29fdd6db6dce56ae668b7f420bf23ac0cf0cd
Raman, Manjunathan <Manjunathan.Raman@in.tesco.com>
2016-06-24 12:28:20 +0530

    Update AbstractElasticsearchTestVerticle.java
    DISCO-2857: Changed template from default to browse.default

--- src/test/java/com/tesco/disco/browse/integration/AbstractElasticsearchTestVerticle.java
+++ src/test/java/com/tesco/disco/browse/integration/AbstractElasticsearchTestVerticle.java
@@ -92,8 +92,8 @@ public class AbstractElasticsearchTestVerticle extends AbstractVerticle {
         bulkRequest.refresh(true);
         loadData(bulkRequest, "ghs.products", "product", "productsData/products.json");
 
-        bulkRequest.add(new IndexRequest(".scripts", "mustache", "ghs.products.default").source(
-                TestingUtilities.buildQuery("src/test/resources/productsTemplate/", "ghs.products.default")));
+        bulkRequest.add(new IndexRequest(".scripts", "mustache", "ghs.products.browse.default").source(
+                TestingUtilities.buildQuery("src/test/resources/productsTemplate/", "ghs.products.browse.default")));
 
         getClient().bulk(bulkRequest).get();
         logger.info("Loaded data into Elasticsearch");
